7238c89b04e9e360c90eef75f61b1b9e72187d4c
DarAnod-B <79657855+DarAnod-B@users.noreply.github.com>
2025-02-13 20:35:25 +0300

    Корректировка табуляций

--- bot/config/config_example.py
+++ bot/config/config_example.py
@@ -5,7 +5,6 @@ BOT_TOKEN = os.getenv(
     "BOT_TOKEN", "..."
 )
 
-#7476334643:AAFzxg72ovKAb_qp3o1kXOTSAMj694Grvt8
 
 MEMORY_LIMIT = 4 * 1024 * 1024 * 1024  # 4GB в байтах
 SWAP_LIMIT = 4 * 1024 * 1024 * 1024  # 4GB Swap
